e83c29a9d4c1ed91757ced56dbc5dff575c4aac6
suyh <suyunhong@ater-info.com>
2025-03-29 15:38:38 +0800

    1

--- src/http/index.js
+++ src/http/index.js
@@ -25,7 +25,7 @@ http.interceptors.response.use(function (response) {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
     //原生的响应对象中的 data 才是服务器返回的数据
-    return response.data; //直接把服务器的数据交给后来的人
+    return response.data.data; //直接把服务器的数据交给后来的人
   }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
--- src/views/cpu/CpuLoad.vue
+++ src/views/cpu/CpuLoad.vue
@@ -2,9 +2,9 @@
   <a-space wrap>
     <!-- 16个 叫 cpu-[1~16] -->
     <div
-        :id="`cpu-${i}`"
-        style="width: 180px; height: 180px; border: 1px solid black"
-        v-for="i in 16"
+        :id="`ts-${i}`"
+        style="width: 800px; height: 800px; border: 1px solid black"
+        v-for="i in 1"
     ></div>
   </a-space>
 </template>
@@ -15,7 +15,7 @@ import {onMounted, ref, onUnmounted} from "vue";
 import {taskManagerListAll} from "@/api/cpuloadApi"
 
 //保存所有初始化的图表
-const chartDom = ref([]);
+const chartDom = ref({});
 let timer;
 
 onMounted(() => {
@@ -33,47 +33,32 @@ onUnmounted(() => {
 });
 
 const initChart = () => {
-  for (let i = 1; i <= 16; i++) {
-    var dom = document.getElementById("cpu-" + i);
-    //得到一个chart对象
-    var myChart = echarts.init(dom);
-    chartDom.value.push(myChart)
-  }
+  let dom = document.getElementById("ts-1");
+  //得到一个chart对象
+  chartDom.value = echarts.init(dom);
 }
 
-// 16核 cpu 的所有数据。
-// 每一核还是一个数组，这个数组中保存的是每秒的数据
 const cpuAllData = ref([])
-
 
 const getCpuData = async () => {
 
   //1、拿到服务器真正的响应； 给服务器发送请求获取
-  let resp = await taskManagerListAll();
-  //2、返回的是16核cpu。每个核心当前的负载值
-  let data = resp.data;
-  for (let i = 0; i < 16; i++) {
-    //把当前请求到的这个cpu的使用率放进自己的数组中。第一次要初始化数组
-    if (!cpuAllData.value[i]) {
-      cpuAllData.value[i] = [];
-    }
+  let data = await taskManagerListAll();
+  console.log("resp: ", data)
 
-
-    if (cpuAllData.value[i].length > 60) {
-      //把最老的一个数据删除，放入最新获取的这个数据
-      let arr = cpuAllData.value[i].slice(-60); //移除了最前一个元素的数组
-      arr.push(data[i])
-      cpuAllData.value[i] = arr;
-    } else {
-      //每次给里面放之前最数组的长度进行一个判断
-      cpuAllData.value[i].push(data[i])
-    }
-
-
-    //这里会 OOM；这个数组最多放 60个？ 超过 60个删除最老的
-
-    drawCpuLoad(i + 1, cpuAllData.value[i])
+  if (cpuAllData.value.length > 60) {
+    //把最老的一个数据删除，放入最新获取的这个数据
+    let arr = cpuAllData.value.slice(-60); //移除了最前一个元素的数组
+    arr.push(data)
+    cpuAllData.value = arr;
+  } else {
+    //每次给里面放之前最数组的长度进行一个判断
+    cpuAllData.value.push(data[i])
   }
+
+  //这里会 OOM；这个数组最多放 60个？ 超过 60个删除最老的
+
+  drawCpuLoad(cpuAllData.value)
 
   // await getCpuData();
 }
@@ -81,15 +66,15 @@ const getCpuData = async () => {
 //1、每个图显示CPU名
 //2、每个图xy轴不显示
 //3、显示为面积图
-const drawCpuLoad = (cpuIndex, cpuData) => {
+const drawCpuLoad = (cpuData) => {
   //得到一个chart对象
-  var myChart = chartDom.value[cpuIndex - 1]
-  var option;
+  let myChart = chartDom.value
+  let option;
 
 //   textStyle. fontSize
 
   option = {
-    title: {text: 'CPU' + cpuIndex, textStyle: {fontSize: 14}},
+    title: {text: 'TaskManagerMetrics', textStyle: {fontSize: 14}},
     grid: {left: "0", right: "0", bottom: "5", top: "0"},
     xAxis: {
       show: false,
